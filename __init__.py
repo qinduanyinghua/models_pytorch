eaf7cad32c502650475babe906078bb8dcb2166b
qinduanyinghua <1090950466@qq.com>
2018-05-11 19:14:39 +0800

    add model vgg network

--- __init__.py
+++ __init__.py
@@ -1,1 +1,2 @@
 from .spatial_pyramid_pooling import SpatialPyramidPooling2d
+from .vgg import VGG
---
+++ vgg.py
@@ -0,0 +1,51 @@
+"""
+vgg 11/13/16/19 in pytorch
+"""
+import torch
+import torch.nn as nn
+
+cfg = {
+    'vgg11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'vgg13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'vgg16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'vgg19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+class VGG(nn.Module):
+    def __init__(self, vgg_name):
+        super(VGG, self).__init__()
+        self.feature = self._make_layers(cfg[vgg_name])
+        self.linear = nn.Sequential(
+            nn.Linear(512, 4096),
+            nn.Linear(4096, 4096),
+            nn.Linear(4096, 1000)
+        )
+    
+    def forward(self, x):
+        out = self.feature(x)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+    
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        for i in cfg:
+            if i == 'M':
+                layers += [nn.MaxPool2d(kernel_size = 2, stride = 2)]
+            else:
+                layers += [nn.Conv2d(in_channels, i, kernel_size=3, padding=1),
+                            nn.BatchNorm2d(i),
+                            nn.ReLU(inplace=True)]
+                in_channels = i
+        layers += [nn.AvgPool2d(kernel_size = 7, stride = 7)]
+        return nn.Sequential(*layers)    
+
+if __name__ == '__main__':
+    net = VGG('vgg11')
+    x = torch.randn(2, 3, 227, 227)
+    out = net(x)
+    print(out, out.shape)
+
+
+
